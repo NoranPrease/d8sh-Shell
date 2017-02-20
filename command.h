1bd4156b76e77f2f8969b8fde4001331cb3c3a08
NoranPrease <nolan.kraynak@gmail.com>
2017-02-20 15:39:40 -0500

    d8sh
    Bash shell with file redirection and piping

---
+++ command.h
@@ -0,0 +1,18 @@
+/* each command to execute is a leaf in this tree. */
+/* commands are tied together in the tree, left side 
+   executed first. */
+struct tree {
+  enum { NONE = 0, AND, OR, SEMI, PIPE } conjunction;
+  struct tree *left, *right;
+  char **argv;
+  char *input;
+  char *output;
+};
+
+/* array of strings to match the conjunction enum
+   above.  print the element using conj[tree->conjunction] */
+/* declared static (improperly) to avoid duplicate symbols;
+   improper because it creates multiple copies of the strings
+   in a binary.  Done here since we might change the operations. */
+   
+static const char *conj[] __attribute__((unused)) = { "err", "&&", "||", ";", "|" };  
